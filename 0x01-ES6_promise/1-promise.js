7670816052156a1cfbc565e7cacfdcd60e6bc6ab
zibxto <christianaziba@gmail.com>
2024-03-07 00:41:40 +0100

    solve task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve) => {
+        if (success === true) {
+            resolve({
+                status: 200,
+                body: 'Success'
+            })
+        }
+        else {
+            throw new Error('The fake API is not working currently');
+        }
+    });
+}

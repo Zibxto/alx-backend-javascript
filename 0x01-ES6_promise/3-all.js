b073f21d3396cabb2301c5bb4d42884f881e7c8f
zibxto <christianaziba@gmail.com>
2024-03-07 02:15:48 +0100

    update file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
-      const [uploadPhoto, createUser] = result;
-      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
+      const [photo, user] = result;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }

be06afc300b2ab304f81e13cbdc42917a7e85bc6
zibxto <christianaziba@gmail.com>
2024-03-07 16:24:10 +0100

    update task 6 solution

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,21 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const s = signUpUser(firstName, lastName);
-  const u = uploadPhoto(fileName);
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
-  return Promise.allSettled([s, u]).then((vals) => {
-    const resArr = [];
-    vals.forEach((val) => {
-      if (val.status === 'fulfilled') {
-        resArr.push({ status: val.status, value: val.value });
-      } else {
-        resArr.push({
-          status: val.status,
-          value: `Error: ${val.reason.message}`,
-        });
-      }
-    });
-    return resArr;
-  });
+  return Promise.allSettled(promises).then((result) => result.forEach((result) => {
+    const arr = [];
+    if (result.status === 'fulfilled') {
+      arr.push({ status: result.status, value: result.value });
+    } else {
+      arr.push({
+        status: result.status,
+        value: `Error: ${result.reason.message}`,
+      });
+    }
+    return arr;
+  }));
 }

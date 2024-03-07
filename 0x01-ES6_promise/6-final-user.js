155300671275656a807b9f0bf58fac382ab930bf
zibxto <christianaziba@gmail.com>
2024-03-07 16:15:46 +0100

    update task 6 solution

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled(promises).then((result) => result.forEach((result) => ([{
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : result.reason.message,
   }])));
 }

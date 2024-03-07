df6ff840808905e3f8f324422c5e371f6bb23ce0
zibxto <christianaziba@gmail.com>
2024-03-07 02:09:11 +0100

    solve task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,9 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       const [uploadPhoto, createUser] = result;
-      console.log(uploadPhoto.body, createUser.firstName, createUser.lastName);
+      console.log(`${uploadPhoto.body} ${createUser.firstName} ${createUser.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }

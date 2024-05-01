2cea22f5a375c0ee61b23f8c17218d9ab1bef127
zibxto <christianaziba@gmail.com>
2024-05-01 18:56:41 +0100

    solve task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port, (error) => {
+  // Checking any error occur while listening on port
+  if (error) {
+    console.log('Something went wrong', error);
+  }
+  // Else sent message of listening
+  else {
+    console.log(`Server is listening on port ${port}`);
+  }
+});

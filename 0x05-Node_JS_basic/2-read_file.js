d2c416ae5d7175e029ab6f41ea414181b7538304
zibxto <christianaziba@gmail.com>
2024-05-01 17:10:22 +0100

    update file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  fs.access(path, fs.constants.F_OK, (err) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    } else {
-      const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
-      const lines = data.split('\n');
-      const numberOfStudents = lines.length - 1;
-      console.log(`Number of students: ${numberOfStudents}`);
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    const lines = data.split('\n');
+    const numberOfStudents = lines.length - 1;
+    console.log(`Number of students: ${numberOfStudents}`);
 
-      const studentsInCS = lines.filter((line) => line.includes('CS'));
-      const numberOfStudentsInCS = studentsInCS.length;
+    const studentsInCS = lines.filter((line) => line.includes('CS'));
+    const numberOfStudentsInCS = studentsInCS.length;
 
-      console.log(`Number of students in CS: ${numberOfStudentsInCS}. List: ${studentsInCS.map((arr) => arr.split(',')[0]).join(', ')}`);
+    console.log(`Number of students in CS: ${numberOfStudentsInCS}. List: ${studentsInCS.map((arr) => arr.split(',')[0]).join(', ')}`);
 
-      const studentsInSWE = lines.filter((line) => line.includes('SWE'));
-      const numberOfStudentsInSWE = studentsInSWE.length;
+    const studentsInSWE = lines.filter((line) => line.includes('SWE'));
+    const numberOfStudentsInSWE = studentsInSWE.length;
 
-      console.log(`Number of students in SWE: ${numberOfStudentsInSWE}. List: ${studentsInSWE.map((arr) => arr.split(',')[0]).join(', ')}`);
-    }
-  });
+    console.log(`Number of students in SWE: ${numberOfStudentsInSWE}. List: ${studentsInSWE.map((arr) => arr.split(',')[0]).join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 };

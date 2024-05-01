1c1b5ebe4b282b0feaf09a9bf322837fd67ba903
zibxto <christianaziba@gmail.com>
2024-05-01 16:53:19 +0100

    solve task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    } else {
+      const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+      const lines = data.split('\n');
+      const numberOfStudents = lines.length - 1;
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      const studentsInCS = lines.filter((line) => line.includes('CS'));
+      const numberOfStudentsInCS = studentsInCS.length;
+
+      console.log(`Number of students in CS: ${numberOfStudentsInCS}. List: ${studentsInCS.map((arr) => arr.split(',')[0]).join(', ')}`);
+
+      const studentsInSWE = lines.filter((line) => line.includes('SWE'));
+      const numberOfStudentsInSWE = studentsInSWE.length;
+
+      console.log(`Number of students in SWE: ${numberOfStudentsInSWE}. List: ${studentsInSWE.map((arr) => arr.split(',')[0]).join(', ')}`);
+    }
+  });
+};

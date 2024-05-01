fefbedf12d2d880937087329bfc26e839e018804
zibxto <christianaziba@gmail.com>
2024-05-01 19:05:28 +0100

    fix linting error

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -9,9 +9,8 @@ const app = http.createServer((req, res) => {
 
 app.listen(port, (error) => {
   // Checking any error occur while listening on port
-  if (error) {
-    console.log('Something went wrong', error);
-  }
+  if (error) console.log('Something went wrong', error);
+
   // Else sent message of listening
   else {
     console.log(`Server is listening on port ${port}`);
